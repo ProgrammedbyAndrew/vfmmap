a5dc11d20cad8d4a225e7b6cd74dbfac85679f00
ProgrammedByAndrew <andrewstockus@gmail.com>
2023-06-12 10:47:46 -0400

    lets try

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'));
